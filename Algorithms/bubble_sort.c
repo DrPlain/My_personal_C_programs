d078a25e9ef4c8819a2a0321904db8940f1a94b2
Obiasor Gideon <gideonobiasor@gmail.com>
2022-10-31 21:24:23 +0000

    Binary search

--- Algorithms/bubble_sort.c
+++ Algorithms/bubble_sort.c
@@ -7,7 +7,7 @@ void improved_bubble_sort(int *array, int size);
 int main(void)
 {
     int size;
-    int array[] = {3,6,1,8,2,9,4, 56, 34, 21, 34, 7, 76, 34};
+    int array[] = {3,6,1,8,2,9,4,56,34,21,34,7,76,34};
 
     size = sizeof(array) / sizeof(array[0]);
     improved_bubble_sort(array, size);
@@ -51,16 +51,19 @@ void improved_bubble_sort(int *array, int size)
  * Return: Nothing
  */
 {
-    int i, j, pass, tmp;
+    int i, j, pass, tmp, flag;
 
     pass = size - 1;
 
         /*Outer loop is for number of passes in n sized array which is 'n - 1'*/
         for (i = 0; i < pass; i++)
         {
+            flag = 0;
             /*Inner loop controls the number of comparisons for each pass*/
-            /*Improvement is on the fact that number of comparisons in each pass*/
+            /*1. Improvement is on the fact that number of comparisons in each pass*/
             /*is == pass - i*/
+            /*2. Loop breaks if array gets sorted before reaching the last pass*/
+
             for (j = 0; j < pass - i; j++)
             {
                 if (array[j] > array[j + 1])
@@ -68,8 +71,11 @@ void improved_bubble_sort(int *array, int size)
                     tmp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = tmp;
+                    flag = 1; /*set to 1 to indicate that swapping has occurred*/
                 }
             }
+            if (flag == 0)
+                break;
         }
         print_array(array, size);
 }
